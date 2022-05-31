1777809b25a8d6f017df673cf897779bc601d432
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-05-31 17:19:41 +0100

    bookings show

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,6 @@ Rails.application.routes.draw do
   resources :dogs do
     resources :bookings, only: %i[new create]
   end
-<<<<<<< HEAD
-  resources :bookings, only: [:destroy]
-=======
   resources :bookings, only: %i[destroy] # does no need to be within dog scope to delete a bookmark
-  
->>>>>>> 9ca2d300d7f23f37328c3d263da3e31d8b96bb8d
   get "my_bookings", to: "bookings#my_bookings", as: :my_bookings
 end

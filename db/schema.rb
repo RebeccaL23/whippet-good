6be3dc51e1e3392433a5e4a732f4141dcf6030fe
Adrian Hards <github@ahards.com>
2022-06-02 17:43:15 +0100

    implement search bar with terrible styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_102723) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_153658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_102723) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "reviews", force: :cascade do |t|

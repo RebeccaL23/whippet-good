31500dd8b334303c10b8d9bbbe0746e72076f556
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-02 15:09:07 +0100

    add map to dog show page and fit map bounds

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -11,6 +11,8 @@ class DogsController < ApplicationController
 
   def show
     @booking = Booking.new
+    @markers = [{ lat: @dog.geocode[0], lng: @dog.geocode[1] }]
+    image_url = helpers.asset_url("wg-icon.png")
   end
 
   def create
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    console.log("MAP")
+    mapboxgl.accessToken = this.apiKeyValue
+    console.log(this.markersValue)
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/rebeccal23/cl3ve49ch000j14s4zi4o1gml"
+    })
+
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    })
+
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+
+    // this.#addMarkersToMap()
+
+  }
+
+  // #addMarkersToMap() {
+  //   this.markersValue.forEach((marker) => {
+  //     new mapboxgl.Marker()
+  //       .setLngLat([ marker.lng, marker.lat ])
+  //       .addTo(this.map)
+  //   })
+  // }
+}
